0a3c90d075d2bd4f5f6407a4e8c44e12c5c3f0f8
BAS <basburnaarwet@gmail.com>
2025-05-02 10:09:20 +0700

    Create script.ts

---
+++ script.ts
@@ -0,0 +1,22 @@
+const app_name = "My app";
+const app_version = "1.0.0";
+const myArray : number [] = [1, 2, 3];
+let myArray2: number[] = [1, 2, 3];
+
+function sayHello()
+{
+    if(true)
+    {
+        var myName : string = "Bas";
+        var myAge  : number; 
+        var canVote = true;
+        var i;
+
+    }
+    console.log(myArray2);
+
+
+
+
+}
+sayHello()
